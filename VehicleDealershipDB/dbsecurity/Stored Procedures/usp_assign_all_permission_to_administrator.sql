71f392d12d05d08880a82b7527b87bdbbb906b61
Teh Boon Hock <tehboonhock@hotmail.com>
2019-07-01 23:11:03 +0800

    minor update

---
+++ VehicleDealershipDB/dbsecurity/Stored Procedures/usp_assign_all_permission_to_administrator.sql
@@ -0,0 +1,34 @@
+﻿-- =============================================
+-- Author:		hock
+-- Create date: 1.7.2019
+-- Description:	assign all permission to administrator
+-- =============================================
+CREATE PROCEDURE dbsecurity.usp_assign_all_permission_to_administrator 
+	-- Add the parameters for the stored procedure here
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+
+INSERT INTO dbsecurity.usergroup_permission
+
+SELECT 
+	UG.usergroup, 
+	P.permission
+FROM dbsecurity.usergroup UG
+
+JOIN dbsecurity.permission P
+	ON 1=1
+
+WHERE UG.[usergroup] = 'ADMINISTRATOR'
+	AND NOT EXISTS (
+		SELECT *
+		FROM dbsecurity.usergroup_permission UGP
+		WHERE UG.usergroup = UGP.usergroup
+			AND P.[permission] = UGP.permission
+	)
+
+END
