dcf6c87d7d7786956a0eb088a7b8622da6329bf5
Teh Boon Hock <tehboonhock@hotmail.com>
2019-07-26 07:24:06 +0800

    added sp_select_vehicle_group_n_model

---
+++ VehicleDealershipDB/veh/Stored Procedures/sp_select_vehicle_group_n_model.sql
@@ -0,0 +1,32 @@
+﻿-- =============================================
+-- Author:		hock
+-- Create date: 26.7.2019
+-- Description:	select vehicle group and its models
+-- =============================================
+create PROCEDURE veh.[sp_select_vehicle_group_n_model] 
+	-- Add the parameters for the stored procedure here
+	@vgroup INT
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+SELECT
+	VGROUP.[vehicle_group],
+	VGROUP.[vehicle_group_name],
+	VGROUP.[vehicle_brand],
+	VMODEL.[vehicle_model],
+	VMODEL.[vehicle_model_name]
+
+FROM [veh].[vehicle_group] VGROUP
+
+LEFT JOIN [veh].[vehicle_model] VMODEL
+	ON VGROUP.[vehicle_group] = VMODEL.[vehicle_group]
+
+WHERE VGROUP.[vehicle_group] = @vgroup
+
+
+
+END
